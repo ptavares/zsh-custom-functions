3d8e6aa887400bf62ac808d46d7f8c3b675bf09d
Patrick Tavares <tavarespatrick01@gmail.com>
2020-07-02 23:08:45 +0200

    :tada: initial commit
    initial zsh custom functions

---
+++ README.md
@@ -0,0 +1,68 @@
+# zsh-custom-functions
+
+zsh plugin for loading my personal zsh custom functions
+
+## Table of content
+
+- [zsh-custom-functions](#zsh-custom-functions)
+  - [Usage](#usage)
+  - [Updating custom-functions](#updating-custom-functions)
+  - [License](#license)
+
+## Usage
+
+Once the plugin installed, `custom-functions` will be available
+
+- Using [Antigen](https://github.com/zsh-users/antigen)
+
+Bundle `zsh-custom-functions` in your `.zshrc`
+
+```shell script
+antigen bundle ptavares/zsh-custom-functions
+```
+
+- Using [zplug](https://github.com/b4b4r07/zplug)
+
+Load `zsh-custom-functions` as a plugin in your `.zshrc`
+
+```shell script
+zplug "ptavares/zsh-custom-functions"
+```
+
+- Using [zgen](https://github.com/tarjoilija/zgen)
+
+Include the load command in your `.zshrc`
+
+```shell script
+zget load ptavares/zsh-custom-functions
+```
+
+- As an [Oh My ZSH!](https://github.com/robbyrussell/oh-my-zsh) custom plugin
+
+Clone `zsh-custom-functions` into your custom plugins repo and load as a plugin in your `.zshrc`
+
+```shell script
+git clone https://github.com/ptavares/zsh-custom-functions.git ~/.oh-my-zsh/custom/plugins/zsh-custom-functions
+```
+
+```shell script
+plugins+=(zsh-custom-functions)
+```
+
+Keep in mind that plugins need to be added before `oh-my-zsh.sh` is sourced.
+
+- Manually
+
+Clone this repository somewhere (`~/.zsh-custom-functions` for example) and source it in your `.zshrc`
+
+```shell script
+git clone https://github.com/ptavares/zsh-custom-functions ~/.zsh-custom-functions
+```
+
+```shell script
+source ~/.zsh-custom-functions/zsh-custom-functions.plugin.zsh
+```
+
+## License
+
+[MIT](LICENCE)
---
+++ zsh-custom-functions.zsh
@@ -0,0 +1,83 @@
+#!/bin/zsh
+# Custom plugin for zsh
+#
+# Personal custom functions
+#
+# Author : Patrick Tavares <tavarespatrick01@gmail.com>
+#
+
+### Commons ###
+echoArg() {
+  echo "> $1"
+}
+
+
+### Command enhancement functions ###
+.1() { cd ../ ; }                                  # Go back 1 directory level
+.2() { cd ../../ ; }                               # Go back 2 directory levels
+.3() { cd ../../../ ; }                            # Go back 3 directory levels
+.4() { cd ../../../../ ; }                         # Go back 4 directory levels
+.5() { cd ../../../../../ ; }                      # Go back 5 directory levels
+.6() { cd ../../../../../../ ; }                   # Go back 6 directory levels
+root() { sudo su - ; }                             # root         : Switch to root user using sudo
+nHere() { nautilus . ; }                           # nHere        : Opens current directory in Nautilus
+n() { nautilus ; }                                 # n            : Opens a Nautilus
+c() { clear ; }                                    # c            : Clear terminal display
+path() { echo -e ${PATH//:/\\n} ; }                # path         : Echo all executable Paths
+show_options() { shopt ; }                         # Show_options : Display bash options settings
+# ducks() { du -cksh * | sort -hr | head -n 15}      # ducks        : List by size directories
+mkdirCd() {                                        # mkdirCd      : Makes new Dir and jumps inside
+    if (( $# != 1 ))
+      then
+        echoArg "Usage:  $0 [dirName]"
+    else
+        mkdir -p "$1" && cd "$1" ;
+    fi
+}
+
+extract() {i                                       # extract      : Extract common arch files
+  if [ -f $1 ] ; then
+    case $1 in
+      *.tar.bz2)   tar xvjf $1       ;;
+      *.tar.gz)    tar xvzf $1       ;;
+      *.bz2)       bunzip2 -v $1     ;;
+      *.rar)       unrar x $1        ;;
+      *.gz)        gunzip -v $1      ;;
+      *.tar)       tar xvf $1        ;;
+      *.tbz2)      tar xvjf $1       ;;
+      *.tgz)       tar xvzf $1       ;;
+      *.zip)       unzip -v $1       ;;
+      *.Z)         uncompress -v $1  ;;
+      *.7z)        7z x $1           ;;
+      *)     echo "'$1' cannot be extracted via extract()" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
+}
+
+### Functions (network) ###
+# Public ip
+myip() { curl -s ifconfig.me }
+# Private ip
+ipLocal() {
+  for i in $(hostname -I); do
+    name=$(ip a | grep $i | grep -oE '[^ ]+$')
+    mac=$(ip -o link | grep $name | awk '$2 != "lo:" {print $(NF-2)}')
+    echo "$name - ip : $i - mac : $mac"
+  done
+}
+httpDebug() { /usr/bin/curl $@ -o /dev/null -w "dns: %{time_namelookup} connect: %{time_connect} pretransfer: %{time_pretransfer} starttransfer: %{time_starttransfer} total: %{time_total}\n" ; }
+
+### Functions (system) ###
+my_ps() { ps $@ -u $USER -o pid,%cpu,%mem,start,time,bsdtime,command ; }
+infos() {
+  echo -e "Currently logged on        : ${HOST}"
+  echo -e "Additional information:    : $(uname -a)"
+  echo -e "Current date:              : $(date)"
+  echo -e "Machine stats:             : $(uptime)"
+  echo -e "Public facing IP Address   : $(myip)"
+  echo -e "Users logged on:           : \n$(w -h|sed "s/^/\t\t\t     /")"
+  echo
+}
+
